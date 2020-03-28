9a6b9d16f5e69e12ef57fd42acb2b2bbd6a6b5d7
Ved <beheraved@gmail.com>
2020-03-28 22:03:05 +0530

    Singleton

---
+++ Singleton/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var x = MySingleTon.GetInstance;
+            x.PrintData("Hello");
+        }
+    }
+
+    public class MySingleTon
+    {
+        private static MySingleTon Instance = null;
+        public static MySingleTon GetInstance
+        {
+            get
+            {
+                if (Instance == null)
+                    Instance = new MySingleTon();
+                return Instance;
+            }
+        }
+        private MySingleTon()
+        {
+            
+        }
+        public void PrintData(string str)
+        {
+            Console.WriteLine(str);
+        }
+    }
+}

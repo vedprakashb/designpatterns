23d43183096bb07843f8999a5743ec849418f157
Ved <beheraved@gmail.com>
2020-04-01 23:45:21 +0530

    ILoggerFactory - 2

---
+++ Factory/FactoryDesignTwo.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Factory
+{
+    public interface ILoggerProvider
+    {
+        public ILogger GetLogger();
+    }
+    public interface ILogger
+    {
+        public string Value { get; set; }
+        public void Log();
+    }
+
+    public class DatabaseLoggerProvider : ILoggerProvider
+    {
+        public ILogger GetLogger()
+        {
+            return new DataBaseLogger();
+        }
+    }
+    public class FileLoggerProvider : ILoggerProvider
+    {
+        public ILogger GetLogger()
+        {
+            return new FileLogger();
+        }
+    }
+    public class DataBaseLogger : ILogger
+    {
+        public string Value { get ; set; }
+
+        public void Log()
+        {
+            Console.WriteLine($"Log { Value } to Database");
+        }
+    }
+    public class FileLogger : ILogger
+    {
+        public string Value { get ; set; }
+
+        public void Log()
+        {
+            Console.WriteLine($"Log { Value } to file ");
+        }
+    }
+
+}
--- Factory/Program.cs
+++ Factory/Program.cs
@@ -6,11 +6,22 @@ namespace Factory
     {
         static void Main(string[] args)
         {
-            var paidProvider = new PaidUserProvider().GetChart();
-            paidProvider.PlotChart();
+            //var paidProvider = new PaidUserProvider().GetChart();
+            //paidProvider.PlotChart();
 
-            var freeProvider = new FreeUserProvider().GetChart();
-            freeProvider.PlotChart();
+            //var freeProvider = new FreeUserProvider().GetChart();
+            //freeProvider.PlotChart();
+
+
+            DatabaseLoggerProvider dp = new DatabaseLoggerProvider();
+            ILogger dblogger = dp.GetLogger();
+            dblogger.Value = "Database Logger sample";
+
+            dblogger.Log();
+            FileLoggerProvider fp = new FileLoggerProvider();
+            ILogger fileLogger = fp.GetLogger();
+            fileLogger.Value = "File Logger sample";
+            fileLogger.Log();
             Console.ReadLine();
         }
     }

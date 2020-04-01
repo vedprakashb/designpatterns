a6c56205f16e36872ae53f13b1be0fa4f4bf0510
Ved <beheraved@gmail.com>
2020-04-01 23:29:56 +0530

    Facotry version 1 added.

---
+++ Factory/FactoryDesignBasic.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Factory
+{
+
+    public interface IChartFactory
+    {
+        public IChart GetChart();
+    }
+    public interface IChart
+    {
+        public void PlotChart();
+    }
+    public class BarChart : IChart
+    {
+        public void PlotChart()
+        {
+            Console.WriteLine("BarChart plotted");
+        }
+    }
+    public class PiChart : IChart
+    {
+        public void PlotChart()
+        {
+            Console.WriteLine("Pichart Plotted");
+        }
+    }
+
+    public class PaidUserProvider : IChartFactory
+    {
+        public IChart GetChart()
+        {
+            return new PiChart();
+        }
+    }
+    public class FreeUserProvider : IChartFactory
+    {
+        public IChart GetChart()
+        {
+            return new BarChart();
+        }
+    }
+}
---
+++ Factory/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Factory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var paidProvider = new PaidUserProvider().GetChart();
+            paidProvider.PlotChart();
+
+            var freeProvider = new FreeUserProvider().GetChart();
+            freeProvider.PlotChart();
+            Console.ReadLine();
+        }
+    }
+
+}

7100b8184b430d96f233716ae4b41b8e05fd8687
Ved <beheraved@gmail.com>
2020-04-03 17:07:25 +0530

    Abstract factory draft

---
+++ AbstractFactory/AbstractFactory.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Data.OleDb;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractFactory
+{
+    public class DataBaseHelper
+    {
+
+    }
+    public interface IDataBaseFactory
+    {
+        DbConnection GetConnection();
+        DbCommand GetCommand();
+        
+
+    }
+    public class SQLFacotry : IDataBaseFactory
+    {
+        public DbCommand GetCommand()
+        {
+            return new SqlCommand();
+        }
+
+        public DbConnection GetConnection()
+        {
+            return new SqlConnection();
+        }
+    }
+    public class OracleFactory : IDataBaseFactory
+    {
+        public DbCommand GetCommand()
+        {
+            return new OleDbCommand();
+        }
+
+        public DbConnection GetConnection()
+        {
+            return new OleDbConnection();
+        }
+    }
+}
